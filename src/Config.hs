0ed403f9896870b02127aee82c13f1c5a3da5407
Sam Coy <samcoy3@gmail.com>
2020-05-06 21:08:09 +0100

    Staged bot config with no secrets

---
+++ src/Config.hs
@@ -0,0 +1,18 @@
+module Config where
+
+import qualified Data.Map as M
+
+import Discord
+import Discord.Types
+
+botToken :: Text
+botToken = undefined
+
+rulesChannel :: ChannelId
+rulesChannel = undefined
+
+motionsChannel :: ChannelId
+motionsChannel = undefined
+
+players :: [UserId]
+players = undefined
